462bd1c8dfcab2f0fcee43e6a6dc6c8f127b1b11
niculas1989 <nicolas.maranzano@libero.it>
2022-03-15 17:07:31 +0100

    added more changes for style

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,62 +1,56 @@
   <div>
       <div id="upper-footer">
-          <div class="row d-flex align-items-center container">
-              <div class="col-4">
-                  <section>
-                      <div class="row mt-2">
-                          <div class="col-4">
-                              <div>
-                                  <h6>DC COMICS</h6>
-                                  <ul>
-                                      <li>
-                                          <a href="#">Characters</a>
-                                          <a href="#">Movies</a>
-                                          <a href="#">Tv</a>
-                                          <a href="#">Games</a>
-                                          <a href="#">Collectibles</a>
-                                          <a href="#">Videos</a>
-                                          <a href="#">News</a>
-                                      </li>
-                                  </ul>
-                              </div>
-                              <div>
-                                  <h6>SHOP</h6>
-                                  <ul>
-                                      <li>
-                                          <a href="#">Shop DC</a>
-                                          <a href="#">Shop DC Collectibles</a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </div>
-                          <div class="col-4">
-                              <h6>DC</h6>
-                              <ul>
-                                  <li><a href="#">Terme Of Use</a></li>
-                                  <li><a href="#">Privacy policy (New)</a></li>
-                                  <li><a href="#">Movies</a></li>
-                                  <li><a href="#">Ad Choices</a></li>
-                                  <li><a href="#">Advertising</a></li>
-                                  <li><a href="#">Jobs</a></li>
-                                  <li><a href="#">Talent Workshops</a></li>
-                                  <li><a href="#">CPSC Certificates</a></li>
-                                  <li><a href="#">Ratings</a></li>
-                                  <li><a href="#">Shop Help</a></li>
-                                  <li><a href="#">Contact Us</a></li>
-                              </ul>
-                          </div>
-                          <div class="col-4">
-                              <h6>SITES</h6>
-                              <ul>
-                                  <li><a href="#">DC</a></li>
-                                  <li><a href="#">MAD Magazine</a></li>
-                                  <li><a href="#">DC Kids</a></li>
-                                  <li><a href="#">DC Universe</a></li>
-                                  <li><a href="#">DC Pover Visa</a></li>
-                              </ul>
-                          </div>
-                      </div>
-                  </section>
+          <div id="wrap-foot" class="container">
+              <div id="first-col">
+                  <div>
+                      <h6>DC COMICS</h6>
+                      <ul>
+                          <li>
+                              <a href="#">Characters</a>
+                              <a href="#">Movies</a>
+                              <a href="#">Tv</a>
+                              <a href="#">Games</a>
+                              <a href="#">Collectibles</a>
+                              <a href="#">Videos</a>
+                              <a href="#">News</a>
+                          </li>
+                      </ul>
+                  </div>
+                  <div>
+                      <h6>SHOP</h6>
+                      <ul>
+                          <li>
+                              <a href="#">Shop DC</a>
+                              <a href="#">Shop DC Collectibles</a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div>
+                  <h6>DC</h6>
+                  <ul>
+                      <li><a href="#">Terme Of Use</a></li>
+                      <li><a href="#">Privacy policy (New)</a></li>
+                      <li><a href="#">Movies</a></li>
+                      <li><a href="#">Ad Choices</a></li>
+                      <li><a href="#">Advertising</a></li>
+                      <li><a href="#">Jobs</a></li>
+                      <li><a href="#">Talent Workshops</a></li>
+                      <li><a href="#">CPSC Certificates</a></li>
+                      <li><a href="#">Ratings</a></li>
+                      <li><a href="#">Shop Help</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                  </ul>
+              </div>
+              <div>
+                  <h6>SITES</h6>
+                  <ul>
+                      <li><a href="#">DC</a></li>
+                      <li><a href="#">MAD Magazine</a></li>
+                      <li><a href="#">DC Kids</a></li>
+                      <li><a href="#">DC Universe</a></li>
+                      <li><a href="#">DC Pover Visa</a></li>
+                  </ul>
               </div>
           </div>
       </div>
