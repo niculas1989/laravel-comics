b860befa404f4f74463ba28efd3ce7930a3dd35f
niculas1989 <nicolas.maranzano@libero.it>
2022-03-15 14:30:24 +0100

    added a few more changes

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="container">
+        <div id="nav">
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="" />
+            </figure>
+            <ul>
+                <li>
+                    <a href="">Characters</a>
+                    <a href="">Movies</a>
+                    <a href="">Tv</a>
+                    <a href="">Games</a>
+                    <a href="">Collectibles</a>
+                    <a href="">Videos</a>
+                    <a href="">Fans</a>
+                    <a href="">News</a>
+                    <a href="">Shop</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
