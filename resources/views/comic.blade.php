c00fdb44b60a6f22e284462e9221f8d1b57804b0
niculas1989 <nicolas.maranzano@libero.it>
2022-03-16 14:30:55 +0100

    added first steps for style

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -5,7 +5,17 @@
     <jumbotron id="content w-100">
         <div id="top-jumbotron" class="w-100"></div>
         <div id="single-comic">
-            <h1>{{ $comic['title'] }}</h1>
+            <div class="container">
+                <div class="col-left">
+                    <h2>{{ $comic['title'] }}</h2>
+                </div>
+                <div class="col-right">
+                    <span>ADVERTISEMENT</span>
+                    <figure>
+                        <img src="{{ asset('img/adv.jpg')" alt="Logo pubblicitario">
+                    </figure>
+                </div>
+            </div>
         </div>
     </jumbotron>
 
