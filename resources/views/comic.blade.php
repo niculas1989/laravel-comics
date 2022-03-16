ec6eeeec78dae97c0526bf525705b086dc039c8a
niculas1989 <nicolas.maranzano@libero.it>
2022-03-16 14:58:05 +0100

    added more steps

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -30,6 +30,33 @@
                 </div>
             </div>
         </div>
+        <div id="specify-content">
+            <div class="container">
+                <div id="talent">
+                    <h5>Talent</h5>
+                    <p>Art by:
+
+                        @foreach ($comic['artists'] as $artist)
+                        <a href="#">
+                            {{ $artist }},
+                        </a>
+                        @endforeach
+                    </p>
+
+                    <p>Written by:
+
+                        @foreach ($comic['writers'] as $writer)
+                        <a href="#">
+                            {{ $writer }},
+                        </a>
+                        @endforeach
+                    </p>
+                </div>
+                <div id="specs">
+                    <h5>Specs</h5>
+                </div>
+            </div>
+        </div>
     </jumbotron>
 
 </div>
