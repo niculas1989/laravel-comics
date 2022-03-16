533ae738fd75b3063a7ec38cae46ebf1abe36015
niculas1989 <nicolas.maranzano@libero.it>
2022-03-16 13:46:57 +0100

    added another try - all works

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,7 +4,13 @@
 <div>
     <jumbotron id="content w-100">
         <div id="top-jumbotron" class="w-100"></div>
-        <div id="single-comic"></div>
+        <div id="single-comic">
+            @foreach ($comics as $comic)
+            <h1>{{ $comic['title'] }}</h1>
+
+
+            @endforeach
+        </div>
     </jumbotron>
 
 </div>
